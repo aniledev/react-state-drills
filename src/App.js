8b37b22b430c9c959fed32631689128859ae6407
aniledev <elinamcgill@gmail.com>
2020-12-24 21:53:34 -0500

    feat: render HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import HelloWorld from "./HelloWorld";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
 }
 
 export default App;

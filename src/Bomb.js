0d7ca360aeb810ecb5cfc0d0df445c44f935cd2b
aniledev <elinamcgill@gmail.com>
2020-12-24 22:23:16 -0500

    feat: initialize state within Bomb component

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
   render() {
     return (
       <div className="Bomb">
         <h2>Bomb Component</h2>
+        <p>{/*Tick, Tock or BOOM!*/}</p>
       </div>
     );
   }

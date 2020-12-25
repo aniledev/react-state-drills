7b1d6acc96d622341d42a685890e49d9ce702106
aniledev <elinamcgill@gmail.com>
2020-12-24 21:57:56 -0500

    feat: display hello, world text using this.state

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      who: "world",
+    };
+  }
+
   render() {
     return (
       <div className="HelloWorld">
         <h2>HelloWorld Component</h2>
+        <p>This text is updated using state: Hello, {this.state.who}.</p>
       </div>
     );
   }

6c436cea03a6c9427543a12e432fce0bdd1d3508
aniledev <elinamcgill@gmail.com>
2020-12-24 22:02:44 -0500

    feat: add non-functional button jsx to HelloWorld component

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -13,6 +13,9 @@ export default class HelloWorld extends React.Component {
       <div className="HelloWorld">
         <h2>HelloWorld Component</h2>
         <p>This text is updated using state: Hello, {this.state.who}.</p>
+        <button>World</button>
+        <button>Friend</button>
+        <button>React</button>
       </div>
     );
   }

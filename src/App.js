32795858e2c9140d97107abd06d8c081f3a1d8b2
aniledev <elinamcgill@gmail.com>
2020-12-27 02:38:07 -0500

    feat: pass down props from App to Accordion

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import RouletteGun from "./RouletteGun";
 import Accordion from "./Accordion";
 
 function App() {
+  // create prop to pass down to Accordion component
+  const sectionsProp = [
+    {
+      title: "Section 1",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Section 2",
+      content:
+        "Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!",
+    },
+    {
+      title: "Section 3",
+      content:
+        "Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?",
+    },
+  ];
+
   return (
     <div className="App">
       <HelloWorld />
       <Bomb />
       <RouletteGun bulletInChamber={8} />
-      <Accordion />
+      <Accordion sections={sectionsProp} />
     </div>
   );
 }

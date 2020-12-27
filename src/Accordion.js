29890b6080f88604cb7b8a1ee0bbaad6b22bd379
aniledev <elinamcgill@gmail.com>
2020-12-27 11:35:30 -0500

    feat: update renderSectionContent method to conditionally render section content

--- src/Accordion.js
+++ src/Accordion.js
@@ -14,24 +14,34 @@ export default class Accordion extends React.Component {
   };
 
   // this function shoulder render one list item, not all the list items
+  // when the button is clicked it will call the event handler which will change the set to the index of the clicked button
   renderSectionContent(section, index, activeIndex) {
     return (
       <li className="section-name" key={index}>
         <button type="button" onClick={() => this.handleCurrentSection(index)}>
           {section.title}
         </button>
-        {/* This will contain a conditional rendering statement */}
-        {section.content}
+        {/* This will contain a conditional rendering statement that will conditionally render the content if the activeIndex === the index of the list item */}
+        {activeIndex === index && <p>{section.content}</p>}
       </li>
     );
   }
 
   render() {
+    // use object desctructing to access this.state and this.props
+    const { activeIndex } = this.state;
+    const { sections } = this.props;
+
     return (
       // refractor this method to render the buttons list and also render the renderSectionContent for one list item
       <div className="Accordion">
         <h2>Accordion Component</h2>
-        <ul className="section-list">{this.renderListItems()}</ul>
+        {/* Map over the sections and create the list items*/}
+        <ul className="section-list">
+          {sections.map((section, index) =>
+            this.renderSectionContent(section, index, activeIndex)
+          )}
+        </ul>
       </div>
     );
   }

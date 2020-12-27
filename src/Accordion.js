2bce0c2910fbcded85b976a424a4ffd2a07c4afa
aniledev <elinamcgill@gmail.com>
2020-12-27 02:35:21 -0500

    feat: update JSX in accordion component

--- src/Accordion.js
+++ src/Accordion.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default class Accordion extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Accordion">
+        <h1>Accordion Component</h1>
+      </div>
+    );
   }
 }
